a8ba9cf571c2a65d927338f6809897948f80e20a
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-03-12 09:09:22 -0400

    Removed octaves and stuff

--- core/src/noise_wrapper.cpp
+++ core/src/noise_wrapper.cpp
@@ -1,24 +1,10 @@
 #include "noise_wrapper.h"
 
-constexpr double sum_of_apmlitudes() {
-    double sum {};
-    for (const auto &amp : amplitutes) {
-        sum += amp;
-    }
-    return sum;
-}
-
 double get_raw_noise(const FastNoiseLite &noise, double x, double y) {
     return (noise.GetNoise(x, y) + 1) / 2;
 }
 
 double get_elevation(const FastNoiseLite& noise, double x, double y) {
-    double raw_noise {};
-    for (const auto &amp : amplitutes) {
-        double inverse_amp = 1 / amp;
-        raw_noise += get_raw_noise(noise, x * inverse_amp, y * inverse_amp) * amp;
-    }
-    raw_noise /= sum_of_apmlitudes();
-    return raw_noise;
+    return get_raw_noise(noise, x, y);
 }
 
