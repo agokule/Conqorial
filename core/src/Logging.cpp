df6b7b06d2ac5710b8ed868400763cc8f3bcb4cd
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-03-22 17:58:22 -0400

    Fixed a bug with conqorial_log::dierr not being defined

--- core/src/Logging.cpp
+++ core/src/Logging.cpp
@@ -5,3 +5,5 @@ conqorial_log::debug conqorial_log::dout;
 conqorial_log::debug_error conqorial_log::derr;
 conqorial_log::release_log conqorial_log::rout;
 conqorial_log::release_error conqorial_log::rerr;
+conqorial_log::distribution_error conqorial_log::dierr;
+conqorial_log::distribution_log conqorial_log::diout;
