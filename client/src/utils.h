8466dd5ad82374cf85aaed6ba16bbc72957c5123
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-03-25 16:18:51 -0400

    Added utils.h because I forgot to add it earlier

---
+++ client/src/utils.h
@@ -0,0 +1,33 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "imgui.h"
+
+// utility structure for realtime plot
+// copy-pasted from implot_demo.cpp
+struct ScrollingBuffer {
+    int MaxSize;
+    int Offset;
+    ImVector<ImVec2> Data;
+    ScrollingBuffer(int max_size = 5000) {
+        MaxSize = max_size;
+        Offset  = 0;
+        Data.reserve(MaxSize);
+    }
+    void AddPoint(float x, float y) {
+        if (Data.size() < MaxSize)
+            Data.push_back(ImVec2(x,y));
+        else {
+            Data[Offset] = ImVec2(x,y);
+            Offset =  (Offset + 1) % MaxSize;
+        }
+    }
+    void Erase() {
+        if (Data.size() > 0) {
+            Data.shrink(0);
+            Offset  = 0;
+        }
+    }
+};
+
+#endif //UTILS_H
